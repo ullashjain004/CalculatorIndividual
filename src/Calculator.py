8c46e5bf620f516ea044c20bed4d1297a645da68
Sneha Philip <snehaphilip989@gmail.com>
2019-10-31 15:45:43 -0400

    Task: Added csv files

--- src/Calculator.py
+++ src/Calculator.py
@@ -7,6 +7,10 @@ def addition(a, b):
     b = int(b)
     return a + b
 
+def subtraction(a,b):
+    return b-a
+
+
 class Calculator:
     result = 0
 
@@ -16,4 +20,8 @@ class Calculator:
 
     def add(self, a, b):
         self.result = addition(a, b)
+        return self.result
+
+    def sub(self, a, b):
+        self.result = subtraction(a, b)
         return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -7,16 +7,12 @@ calculator = Calculator()
 class MyTestCase(unittest.TestCase):
 
 
-
-    def test_add(self):
-        self.assertEqual(calculator.add(2,2),4)
-
     def test_add_calculator(self):
         test_input = CsvReader('/src/Addition.csv').data
         for row in test_input:
             self.assertEqual(calculator.add(row['Value 1'], row['Value 2']),int(row['Result']))
 
-
+    
 
 if __name__ == '__main__':
     unittest.main()
